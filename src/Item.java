082515da58c6cc705808b9147bb5242bd3b5f2c5
Norbert Nemeth <nnorb93@gmail.com>
2016-12-30 12:43:53 +0100

    Fix "Jsoup.connect"

--- src/Item.java
+++ src/Item.java
@@ -49,7 +49,10 @@ public class Item {
 		while (retry <= max_retry) {
 			try {
 				System.out.println("GET: " + url);
-				content = Jsoup.connect(url).timeout(30000).userAgent("Mozilla/17.0").get();
+				content = Jsoup.connect(url)
+						.userAgent(
+								"Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+						.referrer("http://www.google.com").timeout(3000).get();
 				break;
 			} catch (Exception ex) {
 				// wait before retry
@@ -200,7 +203,7 @@ public class Item {
 				helpfulVotes, totalVotes, verifiedPurchase, realName, reviewDate, content);
 		return thereview;
 	}
-	
+
 	public ArrayList<Review> getReviews() {
 		return reviews;
 	}
